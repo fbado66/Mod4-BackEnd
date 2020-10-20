69b6986a81e478eb6eea9d96948b40ff1d5c3bc5
fbado66 <fbado66@gmail.com>
2020-10-20 12:30:02 -0400

    updating user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email
+  attributes :id, :first_name, :last_name, :email, :username
   has_many :reservations
+  has_many :reviews
   # has_many :listings, through: :reservations
 
 end
